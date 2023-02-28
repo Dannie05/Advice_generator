4174aef4e5be6176490d81024d3734c8712e1710
Dannie05 <danielnwogu05@gmail.com>
2023-02-28 16:08:39 +0100

    finished project. Made use of typed js for typing animation,

---
+++ script/script.js
@@ -0,0 +1,39 @@
+const advice = document.querySelector('.advice');
+const adviceBtn = document.querySelector('.adviceBtn');
+
+
+adviceBtn.addEventListener('click', fetchAdvice);
+
+function fetchAdvice() {
+    fetch("https://api.adviceslip.com/advice")
+        .then(data => {
+            return data.json();
+        })
+        .then(adviceText => {
+            const adviceID = document.querySelector('.adviceID')
+            adviceID.innerHTML = `Advice #${adviceText.slip.id}`;
+            const typed = new Typed(advice, {
+                strings: ["", `"${adviceText.slip.advice}"`],
+                typeSpeed: 100,
+                BackSpeed: 60,
+                showCursor: false,
+                loop: false,
+            });
+            // advice.innerHTML='"' + adviceText.slip.advice + '."';       
+
+        })
+
+}
+// const typed = new Typed(advice, {
+//     strings: ["", '"hello"', '"world"', '"image"', '"lorem"'],
+//     typeSpeed: 100,
+//     BackSpeed: 60,
+//     loop: false,
+//     showCursor: false,
+// });
+
+
+fetchAdvice();
+setInterval(fetchAdvice, 3000)
+
+
